5056df8e59e0d287a2a09d34abb5135cc6e5d9c7
Caroline <r0615219@student.thomasmore.be>
2017-03-23 16:57:15 +0100

    home.php layout fixed

--- home.php
+++ home.php
@@ -10,17 +10,21 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="css/reset.css">
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/style-home.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+    <!--<link rel="stylesheet" href="css/signup-style.css">-->
 
     <link href="https://fonts.googleapis.com/css?family=Nova+Oval" rel="stylesheet">
 
-    <title>IMDterest</title>
+    <title>IMDterest | Home</title>
 </head>
 <body>
 
 <?php include_once('header.inc.php'); ?>
 
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/npm.js"></script>
 
 </body>
 </html>
