56b3e5d9c08ae5fdb35cd4ce8115048a19ca5530
Caroline <r0615219@student.thomasmore.be>
2017-03-23 16:37:54 +0100

    layout bootstrap

--- signin.php
+++ signin.php
@@ -1,21 +1,21 @@
 <?php
 
-    include_once 'classes/User.php';
+include_once 'classes/User.php';
 
-    if(!empty($_POST)){
-        $user = new User();
-        $user->Username = $_POST['username'];
-        $user->Password = $_POST['password'];
-        
-        //WERKT NOG NIET !
-        if( $user->Login() ){
-            session_start();
-            header('Location: home.php');
-            $_SESSION['user']=$user->Username;
-        } else {
-            echo 'Whoops, something went wrong.';
-        }
+if(!empty($_POST)){
+    $user = new User();
+    $user->Username = $_POST['username'];
+    $user->Password = $_POST['password'];
+
+    //WERKT NOG NIET !
+    if( $user->Login() ){
+        session_start();
+        header('Location: home.php');
+        $_SESSION['user']=$user->Username;
+    } else {
+        echo 'Whoops, something went wrong.';
     }
+}
 ?>
 
 <!doctype html>
@@ -26,38 +26,53 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="css/reset.css">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+    <link rel="stylesheet" href="css/signup-style.css">
 
     <link href="https://fonts.googleapis.com/css?family=Nova+Oval" rel="stylesheet">
 
-    <title>IMDterest | Sign In</title>
+    <title>IMDterest | Sign Up</title>
 </head>
 <body>
-    <section class="container">
 
-        <div class="head">
-            <h1>Welcome to IMDterest!</h1>
-
-            <h2>Please sign in</h2>
+<nav class="navbar navbar-inverse navbar-fixed-top">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">IMDterest</a>
         </div>
-
-        <div class="signup">
-            <form action="" method="POST">
-                <div>
-                    <!--<label for="username">Username</label>-->
-                    <input type="text" name="username" id="username" placeholder="Username">
+        <div id="navbar" class="navbar-collapse collapse">
+            <form class="navbar-form navbar-right" method="post">
+                <div class="form-group">
+                    <input type="text" placeholder="Username" name="username" id="username" class="form-control">
                 </div>
-                <div>
-                    <!--<label for="password">Password</label>-->
-                    <input type="password" name="password" id="password" placeholder="Password">
+                <div class="form-group">
+                    <input type="password" placeholder="Password" name="password" id="password" class="form-control">
                 </div>
-                <div>
-                    <input type="submit" value="Sign in">
-                </div>
+                <button type="submit" class="btn btn-success">Sign in</button>
             </form>
-            <p>Don't have an account yet? <a href="signup.php">Sign up.</a></p>
-        </div>
+        </div><!--/.navbar-collapse -->
+    </div>
+</nav>
 
-    </section>
+<div class="jumbotron">
+    <div class="container">
+        <h1>A creative way to share ideas!</h1>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+        <p>Join our community!</p>
+        <p><a class="btn btn-primary btn-lg" href="signup.php" role="button">Sign up</a></p>
+    </div>
+</div>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/npm.js"></script>
+
 </body>
 </html>
