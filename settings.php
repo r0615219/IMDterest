2380863bf376dd4d8276ce052df5a34c4b2c199e
Lisa9W <lisa@m-release.com>
2017-03-25 16:19:09 +0100

    modal voor wachtwoord en afbeelding

--- settings.php
+++ settings.php
@@ -49,38 +49,76 @@ if (isset($_SESSION['user'])) {
 
             <div class="media" style="margin-top:30px;">
                 <img src="<?php echo $_SESSION['image']; ?>" alt="profile picture">
-                <button class="btn media" data-toggle="modal" data-target="#uploadImage">Change profile picture</button>
-            </div>
+                <button type="button" class="btn media" data-toggle="modal" data-target="#uploadImage">Change profile picture</button>
 
-            <div id="uploadImage" class="modal fade" role="dialog">
-                <div class="modal-dialog">
 
-                    <!-- Modal content-->
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <button type="button" class="close" data-dismiss="modal">&times;</button>
-                            <h4 class="modal-title">Upload profile picture</h4>
+                <div class="modal fade" id="uploadImage" role="dialog">
+                    <div class="modal-dialog">
+
+                        <!-- Modal content-->
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                <h4 class="modal-title">Upload profile picture</h4>
+                            </div>
+                            <div class="modal-body">
+                                <input type="file">
+                            </div>
+                            <div class="modal-footer">
+                                <button type="submit" class="btn btn-default" data-dismiss="modal">Save</button>
+                            </div>
                         </div>
-                        <div class="modal-body">
-                            <p>Some text in the modal.</p>
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                        </div>
+
                     </div>
-
                 </div>
             </div>
 
             <div style="margin-top:30px;">
                 <label for="password" class="col-xs-12">Password</label>
-                <button class="btn btn-link">Change your password</button>
+                <button type="button" class="btn btn-link" data-toggle="modal" data-target="#changePassword">Change your password</button>
+
+                <div class="modal fade" id="changePassword" role="dialog">
+                    <div class="modal-dialog">
+
+                        <!-- Modal content-->
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                <h4 class="modal-title">Change password</h4>
+                            </div>
+                            <div class="modal-body">
+                                <div class="input-group">
+                                    <label for="oldPassword">Old password</label>
+                                    <input type="password" class="form-control" placeholder="Password" name="password" id="password" aria-describedby="basic-addon1">
+                                </div>
+
+                                <div class="input-group">
+                                    <label for="newPassword">New password</label>
+                                    <input type="password" class="form-control" placeholder="Password" name="password" id="password" aria-describedby="basic-addon1">
+                                </div>
+
+                                <div class="input-group">
+                                    <label for="repeatPassword">Repeat new password</label>
+                                    <input type="password" class="form-control" placeholder="Password" name="password" id="password" aria-describedby="basic-addon1">
+                                </div>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="submit" class="btn btn-default" data-dismiss="modal">Save</button>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
             </div>
+
             <button type="submit" class="btn btn-success" style="margin-top: 50px;">Save settings</button>
 
         </form>
     </div>
 </div>
+
+
+
 
 </body>
 </html>
