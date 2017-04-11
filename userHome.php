aaf16deea66f01de9fd93125cec47336a145c2f8
Lisa9W <lisa@m-release.com>
2017-04-11 20:42:08 +0200

    Started Feature 5

--- userHome.php
+++ userHome.php
@@ -1,10 +1,15 @@
-<h1 class="h1">Your topics</h1>
+<!-- HIER KOMEN ALLE POSTS DAT TE MAKEN HEBBEN MET DE TOPICS GEKOZEN DOOR DE USER -->
     <div class="container">
-        <?php
-        //7. session uitlezen in doc = voorlopige test of het werkt
-        foreach ($_SESSION['topics'] as $t):?>
-            <div class="userTopic" style="background-image: url(<?php echo $t->image; ?>);">
-                <h2><?php echo $t->name; ?></h2>
-            </div>
-        <?php endforeach; ?>
+        <h1 class="h1">Your topics</h1>
+        <div style="width: 100%; display: flex;">
+            <?php
+            //7. session uitlezen in doc = voorlopige test of het werkt
+            foreach ($_SESSION['topics'] as $t):?>
+                <div class="userTopic" style="background-image: url(<?php echo $t->image; ?>);">
+                    <h2><?php echo $t->name; ?></h2>
+                </div>
+            <?php endforeach; ?>
+        </div>
+
+        <button type="button" class="btn btn-success add">+</button>
     </div>
