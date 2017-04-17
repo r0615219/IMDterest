22c898d1a2e4d6efad8bf6de7d7f65788224668f
joris <r0297332@student.thomasmore.be>
2017-04-17 22:13:16 +0200

    hartjes doen nu wat ze moeten doen

--- js/likebutton.js
+++ js/likebutton.js
@@ -4,22 +4,30 @@ $(document).ready(function(){
 
 //check for clicking like button
   $(".likeBtn").click(function(){
+    event.preventDefault();
     //checking if already liked or not -> 1) need userID 2) need postID = PHP check
     var id = $(this).parents(".likes").siblings(".userInfo").children(".postId").html().substring(1);
+    var liked
+    var heart =$(this).find("img");
   $.ajax({
     type:"POST",
     url:"./ajax/like.php",
     data:{"id" : id},
     datatype:"html"
-
     })
-    .done(function(liked) {
-      console.log("hi")
-      console.log(liked)
-      console.log("So confused")
-  });
-  });
 
+  .done(function(res) {
+
+      if (res=="1") {
+        console.log(res)
+        $(heart).attr("src", "./images/icons/heart_filled.svg");
+      }
+      else {
+        console.log(res)
+        $(heart).attr("src", "./images/icons/heart.svg");
+      }
+  });
+});
   // $("img", this).attr("src", "./images/icons/heart_filled.svg");
 //End document.ready
 });
