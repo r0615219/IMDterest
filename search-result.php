4f6d263c4e02cf46441db1e6d5e142563984a35f
Caroline <r0615219@student.thomasmore.be>
2017-04-23 19:32:36 +0200

    delete tab

--- search-result.php
+++ search-result.php
@@ -86,7 +86,7 @@
                             <li role="separator" class="divider"></li>
                             <?php if($searchItem['user_ID'] == $_SESSION['userid']): ?>
 
-                                <li><a href="#">Delete</a></li> <!--via ajax post verwijderen + kijken of post van user is-->
+                                <li><a href="#" data-toggle="modal" data-target="#deletePost" type="submit">Delete</a></li>
 
                             <?php endif; ?>
                         </ul>
@@ -182,6 +182,35 @@
         </div>
 
     <?php endif; ?>
+
+
+    <!-- DELETE post -->
+    <div class="modal fade" id="deletePost" role="dialog">
+        <div class="modal-dialog">
+
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    <h4 class="modal-title">Are you sure you want to delete this post?</h4>
+                </div>
+                <div class="modal-body">
+
+                    <form action="" method="post" enctype="multipart/form-data">
+
+                        <h2><?php echo $res->title; ?></h2>
+
+                        <button class="btn btn-default btn-danger" >Delete</button>
+                        <button class="btn btn-default" data-dismiss="modal" >Cancel</button>
+
+                    </form>
+
+                </div>
+
+            </div>
+
+        </div>
+    </div>
+
 
 </body>
 </html>
