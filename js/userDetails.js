3b4ca042bb7f0567418ede2733b9bbbc384587e0
Roel Van Rossen <roelifant@gmail.com>
2017-04-27 17:30:20 +0200

    onclick ajax call

--- js/userDetails.js
+++ js/userDetails.js
@@ -1,7 +1,16 @@
 $(document).ready(function(){
     $("#follow").click(function(){
-        $("#follow").css('background-color', '#56B056');
-        $("#follow").html();
         
+        $.ajax({
+            type:"POST",
+            url:"./ajax/follow.php"
+        }).done(function(response){
+            if( response.code == 500){
+                console.log("ALLES IS VERKEERD");
+            }
+            if( response.code == 200){
+                console.log("update!");
+            }
+        });
     });
 });
