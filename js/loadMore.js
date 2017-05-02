55fd4c1630b75f882b01e0ee6d1b93abe092ae74
Roel Van Rossen <roelifant@gmail.com>
2017-05-02 10:37:29 +0200

    nieuwe queries geschreven voor home en explore

--- js/loadMore.js
+++ js/loadMore.js
@@ -10,10 +10,10 @@ $(document).ready(function(){
             break;
 
         case 'http://localhost/IMDterest/explore.php':
-            query = "SELECT * FROM posts WHERE topics_ID not in (SELECT topics_ID FROM users_topics WHERE users_ID IN (SELECT id FROM users WHERE email = :email)) ORDER BY id DESC LIMIT :position, :limit";
+            query = "SELECT * FROM posts WHERE id not in (SELECT posts.id FROM posts INNER JOIN follows ON posts.user_ID = follows.user INNER JOIN users ON follows.user = users.id WHERE follows.follower = :usersession ) ORDER BY id DESC LIMIT :position, :limit";
             break;
         case 'http://localhost/IMDterest/explore.php#':
-            query = "SELECT * FROM posts WHERE topics_ID not in (SELECT topics_ID FROM users_topics WHERE users_ID IN (SELECT id FROM users WHERE email = :email)) ORDER BY id DESC LIMIT :position, :limit";
+            query = "SELECT * FROM posts WHERE id not in (SELECT posts.id FROM posts INNER JOIN follows ON posts.user_ID = follows.user INNER JOIN users ON follows.user = users.id WHERE follows.follower = :usersession ) ORDER BY id DESC LIMIT :position, :limit";
             break;
     }
     load_contents(track_page, query); //load content
@@ -22,9 +22,9 @@ $(document).ready(function(){
         track_page++; //page number increment everytime user clicks load button
         var query;
         if($(this).hasClass('loadMoreBtnHome')){
-            query = "SELECT * FROM posts WHERE topics_ID in (SELECT topics_ID FROM users_topics WHERE users_ID IN (SELECT id FROM users WHERE email = :email)) ORDER BY id DESC LIMIT :position, :limit";
+            query = "SELECT * FROM posts p INNER JOIN follows f ON p.user_ID = f.user WHERE f.follower = :usersession ORDER BY id DESC LIMIT :position, :limit";
         }else if($(this).hasClass('loadMoreBtnExplore')){
-            query = "SELECT * FROM posts WHERE topics_ID not in (SELECT topics_ID FROM users_topics WHERE users_ID NOT IN (SELECT id FROM users WHERE email = :email)) ORDER BY id DESC LIMIT :position, :limit";
+            query = "SELECT * FROM posts WHERE id not in (SELECT posts.id FROM posts INNER JOIN follows ON posts.user_ID = follows.user INNER JOIN users ON follows.user = users.id WHERE follows.follower = :usersession ) ORDER BY id DESC LIMIT :position, :limit";
         }
         load_contents(track_page, query); //load content
     });
