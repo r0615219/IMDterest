21bcbeb6967c6e109f994f99cbea85cc5bbd7f86
Caroline <r0615219@student.thomasmore.be>
2017-05-04 23:55:45 +0200

    locatie weergeven

--- ajax/location.php
+++ ajax/location.php
@@ -1,5 +1,4 @@
 <?php
-<<<<<<< HEAD
     session_start();
     include_once('../classes/Post.php');
     $location = $_POST['varLocation'];
@@ -8,12 +7,3 @@
     //$locationPost->location = $location;
     //$locationPost->printLocation($location);
     //echo 'Locatie : '.$location;
-=======
-session_start();
-include_once('../classes/Post.php');
-$location = $_POST['varLocation'];
-$locationPost = new Post();
-//$locationPost->location = $location;
-//$locationPost->printLocation($location);
-echo 'De locatie is'.$location;
->>>>>>> a517b75b373d2f78e319cdbcfbe190f1d938e549
