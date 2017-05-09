f35ce51f49907cecdc91e9e6f4c87f17a6b78d56
Lisa9W <lisa@m-release.com>
2017-05-09 22:27:27 +0200

    error handeling when creating posts done

--- post.inc.php
+++ post.inc.php
@@ -1,9 +1,18 @@
 <?php
 // post verwerking
+// VIA IMAGE POSTEN
 try {
     if (isset($_POST['imgSubmit'])) {
-        $title = $_POST['title'];
-        $description = $_POST['imgDescription'];
+        if ($_POST['title'] != '') {
+            $title = $_POST['title'];
+        } else {
+            throw new ErrorException("Please fill in a title.");
+        }
+        if ($_POST['description'] != '') {
+            $description = $_POST['imgDescription'];
+        } else {
+            throw new ErrorException("Please fill in a description.");
+        }
 
         $post = new Post;
         $post->title = $title;
@@ -37,7 +46,7 @@ try {
                 throw new ErrorException("Your image size has to be smaller than 8MB.");
             }
         } else {
-            $post->image = "profile_placeholder.png";
+            throw new ErrorException("Please upload an image.");
         }
 
         if ($_POST['imgTopic'] == 'none') { //indien select niet geselecteerd is
@@ -79,85 +88,100 @@ try {
     $error = $e->getMessage();
 }
 
-if (isset($_POST['linkSubmit'])) {
-    $post = new Post;
-    $link = $_POST['url'];
-    $title = '';
-    $image = '';
-    $description = '';
-    if (strpos($link, 'https://') === false || strpos($link, 'http://') === false) {
-        $link = 'http://' . $link;
-    }
-
-    $html = file_get_contents($link); //get the html returned from the following url
-    $doc = new DOMDocument();
-    libxml_use_internal_errors(TRUE); //disable libxml errors
-
-    if (!empty($html)) { //if any html is actually returned
-        $doc->loadHTML($html);
-        libxml_clear_errors(); //remove errors for yucky html
-        $xpath = new DOMXPath($doc);
-
-//get site's title
-        $nodeTitle = $xpath->query('//title');
-        $title = $nodeTitle[0]->nodeValue;
-//get description
-        if (array_key_exists('description', get_meta_tags($link))) {
-            $description = get_meta_tags($link)['description'];
+// VIA LINK POSTEN
+try {
+    if (isset($_POST['linkSubmit'])) {
+        $post = new Post;
+        if ($_POST['url'] != '') {
+            $link = $_POST['url'];
         } else {
-            $description = $title;
+            throw new ErrorException("Please fill in a link.");
+        }
+        $title = '';
+        $image = '';
+        $description = '';
+        if (strpos($link, 'https://') === false || strpos($link, 'http://') === false) {
+            $link = 'http://' . $link;
         }
 
-//get all found images
-        $nodeImage = $doc->getElementsByTagName('img');
-        for ($i = 0; $i <= count($nodeImage); $i++) {
-            if ($nodeImage[$i]->getAttribute('src') != '') {
-                $image = $nodeImage[$i]->getAttribute('src');
-                if (strpos($nodeImage[$i]->getAttribute('src'), '../') === true) {
-                    $image = str_replace("../", "/", $nodeImage[$i]->getAttribute('src'));
-                }
-                if (strpos($nodeImage[$i]->getAttribute('src'), 'http') === false) {
-                    $image = $link . $image;
-                }
-                $image = str_replace(' ', '%20', $image);
-                break;
-            }
-        }
+        $html = file_get_contents($link); //get the html returned from the following url
+        $doc = new DOMDocument();
+        libxml_use_internal_errors(TRUE); //disable libxml errors
 
-        if ($_POST['linkTopic'] == 'none') { //indien select niet geselecteerd is
-//nieuwe topic opslaan
-            $newTopic = new Topics;
-            $newTopic->name = $_POST['addTopic'];
-            $newTopic->image = $image;
+        if (!empty($html)) { //if any html is actually returned
+            $doc->loadHTML($html);
+            libxml_clear_errors(); //remove errors for yucky html
+            $xpath = new DOMXPath($doc);
 
-            if ($newTopic->checkAvailability() == 'match') {
-                $topicsId = $newTopic->id;
+            //get site's title
+            $nodeTitle = $xpath->query('//title');
+            $title = $nodeTitle[0]->nodeValue;
+            //get description
+            if (array_key_exists('description', get_meta_tags($link))) {
+                $description = get_meta_tags($link)['description'];
             } else {
-                $newTopic->saveTopic();
-//topicId van nieuwe topic ophalen
-                $newTopic->getTopicViaName();
-                $topicsId = $newTopic->id;
+                $description = $title;
             }
 
-            $newTopic->saveUserTopic();
-            array_push($_SESSION['topics'], $newTopic);
-        } else {
-            $topicsId = $_POST['linkTopic'];
+            //get all found images
+            $nodeImage = $doc->getElementsByTagName('img');
+            for ($i = 0; $i <= count($nodeImage); $i++) {
+                if ($nodeImage[$i]->getAttribute('src') != '') {
+                    $image = $nodeImage[$i]->getAttribute('src');
+                    if (strpos($nodeImage[$i]->getAttribute('src'), '../') === true) {
+                        $image = str_replace("../", "/", $nodeImage[$i]->getAttribute('src'));
+                    }
+                    if (strpos($nodeImage[$i]->getAttribute('src'), 'http') === false) {
+                        $image = $link . $image;
+                    }
+                    $image = str_replace(' ', '', $image);
+                    break;
+                }
+            }
+
+            if ($_POST['linkTopic'] == 'none') { //indien select niet geselecteerd is
+                //nieuwe topic opslaan
+                if ($_POST['addTopic'] != '') {//nieuwe topic opslaan
+                    $newTopic = new Topics;
+                    $newTopic->name = $_POST['addTopic'];
+                    $newTopic->image = str_replace(' ', '', strtolower($_FILES['img']['name']));
+
+                    if ($newTopic->checkAvailability() == 'match') {
+                        $topicsId = $newTopic->id;
+                    } else {
+                        $newTopic->image = $post->image;
+                        $newTopic->saveTopic();
+                        //topicId van nieuwe topic ophalen
+                        $newTopic->getTopicViaName();
+                        $topicsId = $newTopic->id;
+                    }
+
+                    $newTopic->saveUserTopic();
+                    array_push($_SESSION['topics'], $newTopic);
+                    var_dump($_SESSION['topics']);
+                } else {
+                    throw new ErrorException("Please select a topic.");
+                }
+            } else {
+                $topicsId = $_POST['linkTopic'];
+            }
+
+            $post->time = time(); //timestamp
+            $post->description = $description;
+            $post->topics_ID = (int)$topicsId;
+            $post->link = $link;
+            $post->image = $image;
+            $post->title = $title;
+            $post->location = $_POST['location'];
+            $post->savePost();
+
+            $user = new User;
+            $user->Email = $_SESSION['user'];
+            $user->getUserPosts();
         }
-
-        $post->time = time(); //timestamp
-        $post->description = $description;
-        $post->topics_ID = (int)$topicsId;
-        $post->link = $link;
-        $post->image = $image;
-        $post->title = $title;
-        $post->location = $_POST['location'];
-        $post->savePost();
-
-        $user = new User;
-        $user->Email = $_SESSION['user'];
-        $user->getUserPosts();
     }
+} catch (Exception $e) {
+    $error = $e->getMessage();
 }
 
 if (isset($_POST['report'])) {
