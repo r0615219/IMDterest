8fc4cede79f7f04f637335ab0374b904d0247ab5
Caroline <r0615219@student.thomasmore.be>
2017-05-10 22:58:03 +0200

    rebase git

--- index.php
+++ index.php
@@ -8,6 +8,9 @@ spl_autoload_register(function ($class) {
 if (!isset($_SESSION['user'])) {
     header('Location: signin.php');
 }
+
+$success = "";
+
 //TOPICS
 try {
     if (isset($_POST['selectedTopics'])) {
@@ -27,6 +30,8 @@ try {
 } catch (Exception $e) {
     $error = $e->getMessage();
 }
+
+
 
 include_once('post.inc.php');
 
@@ -66,6 +71,9 @@ include_once('header.inc.php'); ?>
 <div class="container">
     <?php if (isset($error)) {
         echo "<p class='alert alert-danger'>$error</p>";
+    } ?>
+    <?php if (!empty($success)) {
+        echo "<p class='alert alert-success'>$success</p>";
     } ?>
     <?php
 
