3b14f8b52e01a1b8a463b4f164ef01b0ac95aa51
Lisa9W <lisa@m-release.com>
2017-05-11 02:23:54 +0200

    posts should be fixed again

--- ajax/comments.php
+++ ajax/comments.php
@@ -1,18 +1,34 @@
 <?php
+
 session_start();
+
 include_once("../classes/Db.php");
-include_once("../classes/comment.php");
+
+include_once("../classes/Comment.php");
+
 $postid=$_POST['postid'];
+
 $comment=$_POST['comment'];
+
 $userid=$_SESSION['userid'];
 
+
+
 $c = new Comment;
+
 $c->post_id=$postid;
+
 $c->comment=$comment;
+
 $c->user_id=$userid;
-//Auto-updates? Ooit zegt, maar nu is het mooi geweest;
+
+
 
 //Ajax update on submit
 
+
+
 $c->saveComment();
+
 echo $c->comment;
+
