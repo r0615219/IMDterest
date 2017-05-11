3410aded2bb181e7b80abdfcceb03e53b50a378f
Lisa9W <lisa@m-release.com>
2017-05-11 09:44:21 +0200

    minor fixes again

--- classes/Post.php
+++ classes/Post.php
@@ -260,9 +260,10 @@ class Post
 
     public function saveToBoard($board_id)
     {
+        echo 'hey';
         //console.log("hey");
         $conn = Db::getInstance();
-        $statement = $conn->prepare("INSERT INTO `board_posts`(`post_id`, `board_id`) VALUES (:post_id,:board_id)");
+        $statement = $conn->prepare("INSERT INTO `boardposts`(`post_id`, `board_id`) VALUES (:post_id,:board_id)");
         $statement->bindvalue(":post_id",$this->m_iID);
         $statement->bindvalue(":board_id",$board_id);
         $res=$statement->execute();
@@ -271,7 +272,7 @@ class Post
 
     public function loadToBoard($board_id){
         $conn = Db::getInstance();
-        $statement =$conn->prepare("SELECT * FROM `board_posts` WHERE `board_id` = (:board_id)");
+        $statement =$conn->prepare("SELECT * FROM `boardposts` WHERE `board_id` = (:board_id)");
         $statement->bindvalue(":board_id",$board_id);
         $statement->execute();
         $res=$statement->fetchAll(PDO::FETCH_ASSOC);
